a75e8bb3c421c0bc25e0a40cc7408b7f740286ae
saineshmamgain <saineshmamgain@gmail.com>
2016-02-04 12:40:35 +0530

    Create keys.php

---
+++ keys.php
@@ -0,0 +1,48 @@
+<?php
+	/*
+	 * class for creating keys
+	 */
+	require_once 'connection.php';
+
+	class Keys{
+		private $allowedCharacters;
+		private $subLength;
+		private $separator;
+		private $subLengthCount;
+		public $key;
+
+		public function __construct(){
+			$settings               =(new Connection())->getSettings();
+			$this->allowedCharacters=$settings['allowed_characters'];
+			$this->subLength        =$settings['sub_length'];
+			$this->separator        =$settings['separator'];
+			$this->subLengthCount   =$settings['sub_length_count'];
+			$this->key              =$this->_generateKey();
+
+			return $this->key;
+		}
+
+		function __toString(){
+			return $this->key;
+		}
+
+		private function _generateKey(){
+			$key=[];
+			for($i=0;$i<$this->subLengthCount;$i++){
+				$key[]=$this->_generateRandomSubString();
+			}
+
+			return implode($this->separator,$key);
+		}
+
+		private function _generateRandomSubString(){
+			$charactersLength=strlen($this->allowedCharacters);
+			$randomString    ='';
+			for($i=0;$i<$this->subLength;$i++){
+				$randomString.=$this->allowedCharacters[ rand(0,$charactersLength-1) ];
+			}
+
+			return $randomString;
+		}
+	}
+?>
